530beeaaae0fe9d515619d8b147b4ca3c13f369a
cabby <rauhamaa.tomi@gmail.com>
2025-07-27 14:29:30 +0300

    changed bans db file name

--- src/database/init.ts
+++ src/database/init.ts
@@ -3,7 +3,7 @@ import { createBansTable } from './migrations';
 import path from 'path';
 import fs from 'fs';
 
-const dbPath = path.resolve(__dirname, './db/bans.db');
+const dbPath = path.resolve(__dirname, './db/bans.sqlite');
 fs.mkdirSync(path.dirname(dbPath), { recursive: true });
 
 const db = new Database(dbPath);
